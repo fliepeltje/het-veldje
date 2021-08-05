b299b555e81ead7bfb072024ff47c41b4d36c9c3
fliepeltje <donatas@rasiukevicius.dev>
2021-08-05 20:12:54 +0200

    Implement baserow as database

--- hetveldje/app.py
+++ hetveldje/app.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+
 from hetveldje.types import Dog
 
 app = FastAPI()
@@ -15,5 +16,5 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/dogs", response_class=HTMLResponse)
 async def read_item(request: Request):
     return templates.TemplateResponse(
-        "dog-list.html", {"request": request, "dogs": Dog.from_content()}
+        "dog-list.html", {"request": request, "dogs": Dog.from_api()}
     )
--- hetveldje/types.py
+++ hetveldje/types.py
@@ -1,7 +1,9 @@
-import toml
 import os
-from pydantic import BaseModel
+from datetime import date
 from typing import Literal
+
+import httpx
+from pydantic import BaseModel
 
 
 class FieldDay(BaseModel):
@@ -30,17 +32,38 @@ class Dog(BaseModel):
     dog_type: str
     owner_name: str
     picture: str
+    gender: Literal["M", "F"]
     extra: list[str]
+    dob: date
     times: list[FieldDay]
 
     @classmethod
-    def from_toml_path(cls, filename: str) -> "Dog":
-        with open(f"content/dogs/{filename}", "r") as f:
-            data = toml.load(f)
-        days_data = data.pop("days")
-        times = [FieldDay(**x) for x in days_data]
-        return cls(times=times, **data)
+    def from_result(cls, data: dict) -> "Dog":
+        times_data = {k: v for k, v in data.items() if k.startswith("times_")}
+        times = [
+            FieldDay(
+                day=int(key.rpartition("_")[2]),
+                times=[x.strip() for x in value.split(",")],
+            )
+            for key, value in times_data.items()
+        ]
+        gender = "M" if data["gender"]["value"] == "Mannetje" else "F"
+        dob = date.fromisoformat(data["dob"])
+        return cls(
+            dog_name=data["dog_name"],
+            owner_name=data["owner_name"],
+            dog_type=data["dog_type"],
+            gender=gender,
+            picture=data["picture_url"],
+            dob=dob,
+            extra=[x.strip() for x in data["extra"].split("\n") if x],
+            times=times,
+        )
 
     @classmethod
-    def from_content(cls) -> list["Dog"]:
-        return [cls.from_toml_path(p) for p in os.listdir("content/dogs")]
+    def from_api(cls) -> list["Dog"]:
+        res = httpx.get(
+            "https://api.baserow.io/api/database/rows/table/26193/?user_field_names=true",
+            headers={"Authorization": f"Token {os.environ['BASEROW_API_KEY']}"},
+        ).json()
+        return [cls.from_result(r) for r in res["results"]]
